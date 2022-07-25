bcbfc177245e57a5956328849a8f1cf4da58d6a6
Vistinska Licnost <TotalnoVistinkiMail@pp.mk>
2022-07-25 11:06:33 +0200

    Srediv Outliers samo nez kako vidov od ova strana https://medium.com/analytics-vidhya/outliers-in-data-and-ways-to-detect-them-1c3a5f2c6b1e

--- ClearDup.py
+++ ClearDup.py
@@ -8,11 +8,23 @@ Created on Fri Jul 22 14:41:00 2022
 import pandas as pd
 import numpy as np 
 
+#Sreduvanje so duplikati ama nema dplikati
 Train = pd.read_csv('train.csv')
-Train = Train.drop_duplicates()
-Columns = Train.columns
-for i in Columns:
-    print (Train[i].value_counts(), '\n')
-# print (Train.duplicated())
-# Train = Train.drop_duplicates()
-# print (Train.duplicated())
+Train.drop_duplicates( inplace=True)
+
+#Sreduvanje Outliers
+Q1_Purchase = Train['Purchase'].quantile(0.25)
+Q3_Purchase = Train['Purchase'].quantile(0.75)
+
+IQR_Purchase = Q3_Purchase - Q1_Purchase
+print (Q1_Purchase, Q3_Purchase, IQR_Purchase)
+
+print (Train)
+
+OutlierFilter = np.logical_or(Train['Purchase'] < (Q1_Purchase - 1.5 * IQR_Purchase),
+                    Train['Purchase'] > (Q3_Purchase + 1.5 * IQR_Purchase))
+Outliers = Train [OutlierFilter]
+Train = Train [~OutlierFilter]
+print(Train)
+
+
